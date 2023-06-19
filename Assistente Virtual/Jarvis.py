b3fe9544ed10790a24810b3aa57166005469a931
Robson Brasil <robson.brasil@outlook.com>
2023-06-18 22:00:24 -0400

    Tentando a todo custo criar uma função para pausar o assistente, mas nao sei como proceder!

---
+++ Assistente Virtual/Jarvis.py
@@ -0,0 +1,180 @@
+import speech_recognition as sr
+import pyttsx3
+import paho.mqtt.client as mqtt
+import sys
+import datetime
+import time
+import requests
+import json
+
+sys.stdout.reconfigure(encoding='utf-8')
+
+# Configurações do MQTT
+mqtt_server = "192.168.15.10"
+mqtt_port = 1883
+mqtt_user = "RobsonBrasil"
+mqtt_password = "loboalfa"
+
+# Tópicos MQTT para os relés
+relay_topics = [
+    "ESP32/MinhaCasa/QuartoRobson/Interruptor1/Comando",
+    "ESP32/MinhaCasa/QuartoRobson/Interruptor2/Comando",
+    "ESP32/MinhaCasa/QuartoRobson/Interruptor3/Comando",
+    "ESP32/MinhaCasa/QuartoRobson/Interruptor4/Comando",
+    "ESP32/MinhaCasa/QuartoRobson/Interruptor5/Comando",
+    "ESP32/MinhaCasa/QuartoRobson/Interruptor6/Comando",
+    "ESP32/MinhaCasa/QuartoRobson/Interruptor7/Comando",
+    "ESP32/MinhaCasa/QuartoRobson/Interruptor8/Comando",
+    "ESP32/MinhaCasa/QuartoRobson/Temperatura"
+]
+
+# Dicionário de aliases para os tópicos
+topic_aliases = {
+    "ESP32/MinhaCasa/QuartoRobson/Interruptor1/Comando": "lâmpada forte",
+    "ESP32/MinhaCasa/QuartoRobson/Interruptor2/Comando": "lâmpada fraca",
+    "ESP32/MinhaCasa/QuartoRobson/Interruptor3/Comando": "cooler",
+    "ESP32/MinhaCasa/QuartoRobson/Interruptor4/Comando": "relé 4",
+    "ESP32/MinhaCasa/QuartoRobson/Interruptor5/Comando": "relé 5",
+    "ESP32/MinhaCasa/QuartoRobson/Interruptor6/Comando": "relé 6",
+    "ESP32/MinhaCasa/QuartoRobson/Interruptor7/Comando": "relé 7",
+    "ESP32/MinhaCasa/QuartoRobson/Interruptor8/Comando": "refletor",
+    "ESP32/MinhaCasa/QuartoRobson/Temperatura": "ambiente"
+}
+
+# falar
+texto_fala = pyttsx3.init()
+paused = False
+
+def falar(audio):
+    rate =  texto_fala.getProperty('rate')
+    texto_fala.setProperty('rate', 180)
+    voices = texto_fala.getProperty('voices')
+    texto_fala.setProperty('voice', voices[3].id) # Trocar as vozes
+    texto_fala.say(audio)
+    texto_fala.runAndWait()
+    time.sleep(1.0)  # Adiciona uma pausa de 0,5 segundos após cada fala
+
+def hora():
+    Hora = datetime.datetime.now().strftime("%#H horas e,:%#M minutos,")
+    falar("Agora são,")
+    falar(Hora)
+
+def obter_nome_mes(numero_mes):
+    meses = {
+        1: "Janeiro!",
+        2: "Fevereiro!",
+        3: "Março!",
+        4: "Abril!",
+        5: "Maio!",
+        6: "Junho,",
+        7: "Julho!",
+        8: "Agosto!",
+        9: "Setembro!",
+        10: "Outubro!",
+        11: "Novembro!",
+        12: "Dezembro!"
+    }
+    return meses.get(numero_mes, "Mês inválido")
+
+def data():
+    now = datetime.datetime.now()
+    dia = str(now.day)
+    mes = obter_nome_mes(now.month)
+    ano = str(now.year)
+
+    falar("A data de hoje é....," + dia,)
+    falar("de-----,   " + mes,)
+    falar("de----- ,  " + ano,)
+
+def bem_vindo():
+    falar ("Olá Robson Brasil, seja bem vindo de volta,")
+    hora()
+    data()
+
+    periodo_do_dia = datetime.datetime.now().hour
+
+    if periodo_do_dia >= 6 and periodo_do_dia < 12:
+        falar ("Bom dia mestre,")
+
+    elif periodo_do_dia >= 12 and periodo_do_dia < 18:
+        falar ("Boa tarde mestre,")
+
+    elif periodo_do_dia >= 18 and periodo_do_dia <= 24:
+        falar ("Boa noite mestre,")
+        falar("Jarvis a sua disposição! Diga-me como posso ajudá-lo hoje?,")
+
+    else:
+        falar("Mestre, vá dormir, já é de madrugada!,")
+
+assistant_active = True
+
+def on_message(client, userdata, message):
+    global assistant_active
+    payload = message.payload.decode("utf-8")
+
+    client.on_message = on_message
+
+    client.subscribe("voiceAssistant/enable")
+    client.subscribe("voiceAssistant/voice")
+    client.loop_forever()
+
+def microfone():
+    r = sr.Recognizer()
+
+    with sr.Microphone() as source:
+        r.pause_threshold = 1
+        audio = r.listen(source)
+
+    try:
+        print("Reconhecendo o Comando")
+        command = r.recognize_google(audio, language='pt-BR')
+        print(command)
+
+    except Exception as e:
+        print(e)
+        #falar(" Por favor Repita, não entendi,")
+
+        return "None"
+
+    return command
+
+# Configurar o cliente MQTT
+client = mqtt.Client()
+client.username_pw_set(mqtt_user, mqtt_password)
+client.connect(mqtt_server, mqtt_port, 60)
+
+if __name__ == "__main__":
+
+    bem_vindo()
+
+    while True:
+
+        print("Estou Escutando....")
+
+        if assistant_active:
+            command = microfone().lower()
+
+        if 'como você está' in command:
+            falar("Estou bem, obrigado e você,")
+            falar("o que posso fazer para ajudá-lo mestre,")
+        
+        elif 'hora' in command:
+            hora()
+
+        elif 'data' in command:
+            data()
+
+        for topic in relay_topics:    
+            alias = topic_aliases[topic]
+            if f"acender {alias}" in command:
+                client.publish(topic, "1")
+                falar(f"Ligando o, {alias}")
+
+            elif f"apagar {alias}" in command:
+                client.publish(topic, "0")
+                falar(f"Apagando o, {alias}")
+
+            elif f"temperatura {alias}" in command:
+                client.publish(topic, str_temp_data)
+                client.publish(topic, str_hum_data)
+                falar("Aguarde um momento enquanto eu verifico a temperatura para você: " + str_temp_data)        
--- Assistente Virtual/kivy.py
+++ Assistente Virtual/kivy.py
@@ -1,121 +1,233 @@
-from kivy.app import App
-from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.label import Label
-from kivy.uix.button import Button
-from kivy.uix.textinput import TextInput
-from kivy.clock import Clock
 import speech_recognition as sr
 import pyttsx3
 import paho.mqtt.client as mqtt
+import sys
+import datetime
+import time
+import requests
+import json
+from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.button import Button
+from kivy.uix.label import Label
 
+sys.stdout.reconfigure(encoding='utf-8')
 
-class MainApp(App):
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
-        self.mqtt_server = "192.168.15.10"
-        self.mqtt_port = 1883
-        self.mqtt_user = "RobsonBrasil"
-        self.mqtt_password = "loboalfa"
-        self.relay_topics = [
-            "ESP32/MinhaCasa/QuartoRobson/Interruptor1/Comando",
-            "ESP32/MinhaCasa/QuartoRobson/Interruptor2/Comando",
-            "ESP32/MinhaCasa/QuartoRobson/Interruptor3/Comando",
-            "ESP32/MinhaCasa/QuartoRobson/Interruptor4/Comando",
-            "ESP32/MinhaCasa/QuartoRobson/Interruptor5/Comando",
-            "ESP32/MinhaCasa/QuartoRobson/Interruptor6/Comando",
-            "ESP32/MinhaCasa/QuartoRobson/Interruptor7/Comando",
-            "ESP32/MinhaCasa/QuartoRobson/Interruptor8/Comando",
-        ]
-        self.topic_aliases = {
-            "ESP32/MinhaCasa/QuartoRobson/Interruptor1/Comando": "luz forte",
-            "ESP32/MinhaCasa/QuartoRobson/Interruptor2/Comando": "luz fraca",
-            "ESP32/MinhaCasa/QuartoRobson/Interruptor3/Comando": "cooler",
-            "ESP32/MinhaCasa/QuartoRobson/Interruptor4/Comando": "relé 4",
-            "ESP32/MinhaCasa/QuartoRobson/Interruptor5/Comando": "relé 5",
-            "ESP32/MinhaCasa/QuartoRobson/Interruptor6/Comando": "relé 6",
-            "ESP32/MinhaCasa/QuartoRobson/Interruptor7/Comando": "relé 7",
-            "ESP32/MinhaCasa/QuartoRobson/Interruptor8/Comando": "luz do quarto",
-        }
-        self.recognizer = sr.Recognizer()
-        self.engine = pyttsx3.init()
-        self.client = mqtt.Client()
-        self.assistant_active = True
+# Configurações do MQTT
+mqtt_server = "192.168.15.10"
+mqtt_port = 1883
+mqtt_user = "RobsonBrasil"
+mqtt_password = "loboalfa"
 
+# Tópicos MQTT para os relés
+relay_topics = [
+    "ESP32/MinhaCasa/QuartoRobson/Interruptor1/Comando",
+    "ESP32/MinhaCasa/QuartoRobson/Interruptor2/Comando",
+    "ESP32/MinhaCasa/QuartoRobson/Interruptor3/Comando",
+    "ESP32/MinhaCasa/QuartoRobson/Interruptor4/Comando",
+    "ESP32/MinhaCasa/QuartoRobson/Interruptor5/Comando",
+    "ESP32/MinhaCasa/QuartoRobson/Interruptor6/Comando",
+    "ESP32/MinhaCasa/QuartoRobson/Interruptor7/Comando",
+    "ESP32/MinhaCasa/QuartoRobson/Interruptor8/Comando",
+    "ESP32/MinhaCasa/QuartoRobson/Temperatura"
+]
+
+# Dicionário de aliases para os tópicos
+topic_aliases = {
+    "ESP32/MinhaCasa/QuartoRobson/Interruptor1/Comando": "lâmpada forte",
+    "ESP32/MinhaCasa/QuartoRobson/Interruptor2/Comando": "lâmpada fraca",
+    "ESP32/MinhaCasa/QuartoRobson/Interruptor3/Comando": "cooler",
+    "ESP32/MinhaCasa/QuartoRobson/Interruptor4/Comando": "relé 4",
+    "ESP32/MinhaCasa/QuartoRobson/Interruptor5/Comando": "relé 5",
+    "ESP32/MinhaCasa/QuartoRobson/Interruptor6/Comando": "relé 6",
+    "ESP32/MinhaCasa/QuartoRobson/Interruptor7/Comando": "relé 7",
+    "ESP32/MinhaCasa/QuartoRobson/Interruptor8/Comando": "refletor",
+    "ESP32/MinhaCasa/QuartoRobson/Temperatura": "ambiente"
+}
+
+class KivyApp(App):
+    
     def build(self):
-        layout = BoxLayout(orientation="vertical")
-
-        label = Label(text="Ouvindo...")
-        layout.add_widget(label)
-
-        button = Button(text="Parar Assistente")
-        button.bind(on_press=self.toggle_assistant)
+        layout = BoxLayout(orientation='vertical')
+        button = Button(text='Clique para falar')
+        button.bind(on_press=self.falar_comando)
         layout.add_widget(button)
-
-        Clock.schedule_interval(self.listen_and_execute, 1)
-
-        self.client.username_pw_set(self.mqtt_user, self.mqtt_password)
-        self.client.connect(self.mqtt_server, self.mqtt_port, 60)
-        self.client.on_message = self.on_message
-        self.client.subscribe("voiceAssistant/enable")
-        self.client.subscribe("voiceAssistant/voice")
-        self.client.loop_start()
+        self.label = Label(text='')
+        layout.add_widget(self.label)
 
         return layout
 
-    def toggle_assistant(self, instance):
-        if self.assistant_active:
-            self.assistant_active = False
-            instance.text = "Iniciar Assistente"
-        else:
-            self.assistant_active = True
-            instance.text = "Parar Assistente"
+    def falar(self, audio):
+        rate =  self.texto_fala.getProperty('rate')
+        self.texto_fala.setProperty('rate', 180)
+        voices = self.texto_fala.getProperty('voices')
+        self.texto_fala.setProperty('voice', voices[3].id)  # Trocar as vozes
+        self.texto_fala.say(audio)
+        self.texto_fala.runAndWait()
+        time.sleep(0.5)  # Adiciona uma pausa de 0,5 segundos após cada fala
+
+    def hora(self):
+        Hora = datetime.datetime.now().strftime("%#H horas e,:%#M minutos,")
+        self.falar("Agora são,")
+        self.falar(Hora)
+
+    def obter_nome_mes(self, numero_mes):
+        
+        meses = {
+            
+            1: "Janeiro!",
+            2: "Fevereiro!",
+            3: "Março!",
+            4: "Abril!",
+            5: "Maio!",
+            6: "Junho,",
+            7: "Julho!",
+            8: "Agosto!",
+            9: "Setembro!",
+            10: "Outubro!",
+            11: "Novembro!",
+            12: "Dezembro!"
+        }
+        return meses.get(numero_mes, "Mês inválido")
+
+    def data(self):
+        now = datetime.datetime.now()
+        dia = str(now.day)
+        mes = self.obter_nome_mes(now.month)
+        ano = str(now.year)
+
+        self.falar("A data de hoje é....," + dia,)
+        self.falar("de-----,   " + mes,)
+        self.falar("de----- ,  " + ano,)
+
+    def bem_vindo(self):
+        self.falar("Olá Robson Brasil, seja bem-vindo de volta,")
+        self.hora()
+        self.data()
+
+    periodo_do_dia = datetime.datetime.now().hour
+
+    if periodo_do_dia >= 6 and periodo_do_dia < 12:
+        self.falar("Bom dia mestre,")
+    elif periodo_do_dia >= 12 and periodo_do_dia < 18:
+        self.falar("Boa tarde mestre,")
+    elif periodo_do_dia >= 18 and periodo_do_dia <= 24:
+        self.falar("Boa noite mestre,")
+    else:
+        self.falar("Mestre, vá dormir, já é de madrugada!,")
+    
+    self.falar("Jarvis à sua disposição! Diga-me como posso ajudá-lo hoje?,")
 
     def on_message(self, client, userdata, message):
+        global assistant_active
         payload = message.payload.decode("utf-8")
-        if message.topic == "voiceAssistant/enable":
-            if payload == "2":
-                self.assistant_active = True
-            else:
-                self.assistant_active = True
 
-        if message.topic == "voiceAssistant/voice":
-            self.engine.setProperty("voice", payload)
+        client.on_message = self.on_message
 
-    def listen_and_execute(self, dt):
-        if self.assistant_active:
-            with sr.Microphone() as source:
-                print("Ouvindo...")
-                audio = self.recognizer.listen(source)
+        client.subscribe("voiceAssistant/enable")
+        client.subscribe("voiceAssistant/voice")
+        client.loop_forever()
 
-            try:
-                command = self.recognizer.recognize_google(audio, language='pt-BR')
-                print(f"Você disse: {command}")
-                self.process_command(command)
-            except sr.UnknownValueError:
-                print("Não entendi, por favor repita.")
-            except sr.RequestError:
-                print("Erro ao tentar reconhecer o comando.")
+    def microfone(self):
+        r = sr.Recognizer()
 
-    def process_command(self, command):
-        command = command.lower()
+        with sr.Microphone() as source:
+            r.pause_threshold = 1
+            audio = r.listen(source)
 
-        for i, topic in enumerate(self.relay_topics):
-            alias = self.topic_aliases[topic]
-            if f"ligar {alias}" in command:
-                self.client.publish(topic, "1")
-                self.engine.say(f"Ligando o {alias}.")
-                self.engine.runAndWait()
-                return
+        try:
+            print("Reconhecendo o Comando")
+            command = r.recognize_google(audio, language='pt-BR')
+            print(command)
 
-            if f"apagar {alias}" in command:
-                self.client.publish(topic, "0")
-                self.engine.say(f"Apagando o {alias}")
-                self.engine.runAndWait()
-                return
+        except Exception as e:
+            print(e)
+            # falar(" Por favor Repita, não entendi,")
+            return "None"
 
-        self.engine.say("Comando não reconhecido.")
-        self.engine.runAndWait()
+        return command
 
+    def falar_comando(self, instance):
+        
+        command = self.microfone().lower()
+        
+    if 'como você está' in command:
+        self.falar("Estou bem, obrigado e você,")
+        self.falar("o que posso fazer para ajudá-lo mestre,")
 
-if __name__ == "__main__":
-    MainApp().run()
+    elif 'hora' in command:
+        self.hora()
+
+    elif 'data' in command:
+        self.data()
+
+    elif 'ligar' in command:
+    if 'lâmpada forte' in command:
+        self.publicar_mqtt("ESP32/MinhaCasa/QuartoRobson/Interruptor1/Comando", "Ligar")
+    elif 'lâmpada fraca' in command:
+        self.publicar_mqtt("ESP32/MinhaCasa/QuartoRobson/Interruptor2/Comando", "Ligar")
+    elif 'cooler' in command:
+        self.publicar_mqtt("ESP32/MinhaCasa/QuartoRobson/Interruptor3/Comando", "Ligar")
+    elif 'relé 4' in command:
+        self.publicar_mqtt("ESP32/MinhaCasa/QuartoRobson/Interruptor4/Comando", "Ligar")
+    elif 'relé 5' in command:
+        self.publicar_mqtt("ESP32/MinhaCasa/QuartoRobson/Interruptor5/Comando", "Ligar")
+    elif 'relé 6' in command:
+        self.publicar_mqtt("ESP32/MinhaCasa/QuartoRobson/Interruptor6/Comando", "Ligar")
+    elif 'relé 7' in command:
+        self.publicar_mqtt("ESP32/MinhaCasa/QuartoRobson/Interruptor7/Comando", "Ligar")
+    elif 'refletor' in command:
+        self.publicar_mqtt("ESP32/MinhaCasa/QuartoRobson/Interruptor8/Comando", "Ligar")
+    else:
+        self.falar("Desculpe, não reconheci o dispositivo para ligar.")
+
+    elif 'desligar' in command:
+    if 'lâmpada forte' in command:
+        self.publicar_mqtt("ESP32/MinhaCasa/QuartoRobson/Interruptor1/Comando", "Desligar")
+    elif 'lâmpada fraca' in command:
+        self.publicar_mqtt("ESP32/MinhaCasa/QuartoRobson/Interruptor2/Comando", "Desligar")
+    elif 'cooler' in command:
+        self.publicar_mqtt("ESP32/MinhaCasa/QuartoRobson/Interruptor3/Comando", "Desligar")
+    elif 'relé 4' in command:
+        self.publicar_mqtt("ESP32/MinhaCasa/QuartoRobson/Interruptor4/Comando", "Desligar")
+    elif 'relé 5' in command:
+        self.publicar_mqtt("ESP32/MinhaCasa/QuartoRobson/Interruptor5/Comando", "Desligar")
+    elif 'relé 6' in command:
+        self.publicar_mqtt("ESP32/MinhaCasa/QuartoRobson/Interruptor6/Comando", "Desligar")
+    elif 'relé 7' in command:
+        self.publicar_mqtt("ESP32/MinhaCasa/QuartoRobson/Interruptor7/Comando", "Desligar")
+    elif 'refletor' in command:
+        self.publicar_mqtt("ESP32/MinhaCasa/QuartoRobson/Interruptor8/Comando", "Desligar")
+    else:
+        self.falar("Desculpe, não reconheci o dispositivo para desligar.")
+
+    elif 'temperatura' in command:
+        self.publicar_mqtt("ESP32/MinhaCasa/QuartoRobson/Temperatura", "Obter")
+    elif 'ajuda' in command:
+        self.falar("Você pode me pedir para fazer várias coisas, como:")
+        self.falar("- Perguntar a hora")
+        self.falar("- Perguntar a data")
+        self.falar("- Ligar ou desligar dispositivos, como lâmpadas, cooler e relés")
+        self.falar("- Obter a temperatura do ambiente")
+        self.falar("Experimente fazer alguns comandos!")
+    else:
+        self.falar("Desculpe, não entendi o comando. Por favor, repita.")
+
+    def publicar_mqtt(self, topic, message):
+        client = mqtt.Client()
+        client.username_pw_set(mqtt_user, mqtt_password)
+        client.connect(mqtt_server, mqtt_port, 60)
+        client.publish(topic, message)
+        client.disconnect()
+
+    def on_start(self):
+        self.texto_fala = pyttsx3.init()
+        self.texto_fala.setProperty('rate', 180)
+        self.texto_fala.setProperty('voice', self.texto_fala.getProperty('voices')[3].id)
+        self.bem_vindo()
+
+    def on_stop(self):
+        self.texto_fala.stop()
+
+if __name__ == '__main__':
+KivyApp().run()
